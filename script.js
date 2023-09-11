8f6f3a303af671c75daf47a3de8900ae5f710b7e
paulaa99 <paau.a@hotmail.com>
2023-09-11 19:38:02 -0300

    fetch

---
+++ script.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    fetch("https://jsonplaceholder.typicode.com/comments")
+        .then(response => response.json())
+        .then(data => {
+            
+            data.sort((a, b) => b.id - a.id);
+            
+            const commentsContainer = document.getElementById("comments-container");
+            for (let i = 0; i < 10; i++) {
+                const comment = data[i];
+                const commentElement = document.createElement("div");
+                commentElement.innerHTML = `
+                    <p>Comentario:${comment.body}</p>
+                `;
+                commentsContainer.appendChild(commentElement);
+            }
+        });
+});
