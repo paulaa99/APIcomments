2195abd8da790e50ed417d7a1314d046efefbfc2
mathiasbernardico <mbernardico27@gmail.com>
2023-09-11 21:52:50 -0300

    Stars

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 document.addEventListener("DOMContentLoaded", function() {
 
+    const estrellas = [
+        '<span class="fa fa-star"></span>',
+        '<span class="fa fa-star"></span>',
+        '<span class="fa fa-star"></span>',
+        '<span class="fa fa-star"></span>',
+        '<span class="fa fa-star"></span>',
+      ];
+      function rellenarEstrellas(estrellas, calificacion) {
+        for (let i = 0; i < estrellas.length; i++) {
+          if (i >= calificacion) {
+            estrellas[i] = '<span class="fa fa-star"></span>';
+          } else {
+            estrellas[i] = '<span class="fa fa-star checked"></span>';
+          }
+        }
+        estrellas = estrellas.join("");
+        return estrellas;
+      }
+
     fetch("https://jsonplaceholder.typicode.com/comments")
         .then(response => response.json())
         .then(data => {
@@ -10,18 +29,61 @@ document.addEventListener("DOMContentLoaded", function() {
             for (let i = 0; i < 10; i++) {
                 const comment = data[i];
                 const commentElement = document.createElement("div");
+                const superEstrellas = rellenarEstrellas(estrellas, mostrarEstrellasAleatorias());
                 commentElement.innerHTML = `
                 <p><strong>Comentario:</strong></p>
+                <p>${superEstrellas}</p>
                 <p>${comment.body}</p>
             `;
                 commentsContainer.appendChild(commentElement);
             }
         });
+
+const btn_comentario = document.getElementById("btn_comentario");
+let selectedRating = 1;
+const contenedor_comentarios = document.getElementById("comments-container");
+
+document.querySelectorAll('.star').forEach(star => {
+    star.addEventListener('click', () => {
+        selectedRating = parseInt(star.getAttribute('data-rating'));
+        console.log(selectedRating);
+        highlightStars(selectedRating);
+    });
+});
+
+function highlightStars(rating) {
+    document.querySelectorAll('.star').forEach(star => {
+        const starRating = parseInt(star.getAttribute('data-rating'));
+        if (starRating <= rating) {
+            star.style.color = 'gold';
+        } else {
+            star.style.color = 'black';
+        }
+    });
+}
+btn_comentario.addEventListener('click', function(event) {
+    event.preventDefault();
+    const description = document.getElementById('comment').value;
+    const comentarioDiv = `
+        <div>
+        <p><strong>Comentario:</strong></p>
+        <p>${rellenarEstrellas(estrellas, selectedRating)}</p>
+        <p>${description}</p>
+        </div>
+    `;
+    contenedor_comentarios.innerHTML += comentarioDiv;
+    document.getElementById('comment').value = '';
+    selectedRating = 1;
+    highlightStars(selectedRating);
+
+  });
+  
 });
 
     // Función para obtener una puntuación aleatoria (solo para simulación)
-       const estrellasAleatorias = Math.floor(Math.random() * 5) + 1;
+       const estrellasAleatorias = Math.floor(Math.random()+1);
        
        function mostrarEstrellasAleatorias(){
+        console.log(estrellasAleatorias)
         return estrellasAleatorias;
     }
